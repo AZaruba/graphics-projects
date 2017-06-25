6f4ac9967442430487b95b874bb72887d5c833ba
Andrew Zaruba <A.B.Zaruba@gmail.com>
2017-06-25 16:04:11 -0500

    broken, requires GLAD

---
+++ first3D.cpp
@@ -0,0 +1,52 @@
+//
+//  first3D.cpp
+//  
+//
+//  Created by Andrew Zaruba on 6/25/17.
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int
+main(int argc, char** argv)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* window = glfwCreateWindow(512, 512, "First3D", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "error initializing window\n";
+        glfwTerminate();
+        exit(1);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    exit(0);
+}
+
+void
+framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0,0,width,height);
+}
